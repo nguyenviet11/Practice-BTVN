9d6f77fdc868ab9080b58f12bf15197ddd982e5c
Viet <nguyenvanvietbn1998@gmail.com>
2022-09-07 08:39:00 +0700

    commit
    btvn

---
+++ Practice2_S5-master/Practice2_S5/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice2_S5.Models;
+
+namespace Practice2_S5.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<UserModel> _users = new List<UserModel>();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(_users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            UserModel user =  _users.Find(emp => emp.Idd == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                UserModel u = new UserModel();
+                u.Idd = Int32.Parse(collection["Idd"]);
+                u.ConfirmPassword = collection["ConfirmPassword"];
+                u.Email = collection["email"];  
+                u.Password = collection["password"];
+                u.Name = collection["name"];
+                _users.Add(u);
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Practice2_S5-master/Practice2_S5/Models/UserModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practice2_S5.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public int Idd { get; set; }
+
+
+        [Required(ErrorMessage = "Bạn phải nhập tên")]
+        [StringLength(30, MinimumLength = 6)]
+        [DisplayName("Họ và tên")]
+
+        public string Name { set; get; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Bạn phải nhập password")]
+        [StringLength(30, MinimumLength = 5)]
+        [DisplayName("Mật khẩu")]
+        public string Password { set; get; }
+
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [DisplayName("Re Mật khẩu")]
+        [Required(ErrorMessage = "Bạn phải nhập password")]
+        public string ConfirmPassword { set; get; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Bạn phải nhập email")]
+        [EmailAddress]
+        public string Email { set; get; }
+
+    }
+}
